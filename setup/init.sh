5c8bfcdbb30d2207f11ffa2bd5c29c1b45c61ee6
rahulm1308 <rahulmarria1969@gmail.com>
2024-07-27 18:18:01 +0530

    commit

--- setup/init.sh
+++ setup/init.sh
@@ -4,9 +4,30 @@ set -e -o pipefail
 echo "Fetching IAM github-action-user ARN"
 userarn=$(aws iam get-user --user-name github-action-user | jq -r .User.Arn)
 
+# Detect the operating system and architecture
+OS=$(uname | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+
+# Map the architecture names to the expected ones
+case $ARCH in
+  x86_64)
+    ARCH="amd64"
+    ;;
+  aarch64)
+    ARCH="arm64"
+    ;;
+esac
+
+# Construct the download URL based on the OS and ARCH
+if [[ "$OS" == "mingw"* || "$OS" == "cygwin"* || "$OS" == "msys"* ]]; then
+  DOWNLOAD_URL="https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.2/aws-iam-authenticator_0.6.2_windows_amd64.exe"
+else
+  DOWNLOAD_URL="https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.2/aws-iam-authenticator_0.6.2_${OS}_${ARCH}"
+fi
+
 # Download tool for manipulating aws-auth
-echo "Downloading tool..."
-curl -X GET -L https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.2/aws-iam-authenticator_0.6.2_linux_amd64 -o aws-iam-authenticator
+echo "Downloading tool from ${DOWNLOAD_URL}..."
+curl -X GET -L "${DOWNLOAD_URL}" -o aws-iam-authenticator
 chmod +x aws-iam-authenticator
 
 echo "Updating permissions"
